18301886d833f94a7dd344d18e77dcc46dd1b23f
Tiago Raneia <tiago.ranea93@gmail.com>
2025-09-02 17:41:15 -0300

    continuação ex018

--- CursoJs/aula16ex/ex018/script.js
+++ CursoJs/aula16ex/ex018/script.js
@@ -1,5 +1,5 @@
 let num = document.querySelector('input#fnum')
-let list = document.querySelector('select#flista')
+let lista = document.querySelector('select#flista')
 let res = document.querySelector('div#res')
 let valores = []
 
@@ -22,8 +22,28 @@ function inLista(n, l){
 
 function adicionar () { 
   if(isNumero(num.value) && !inLista(num.value, valores)) {
-    
+    valores.push(Number(num.value))
+    let item = document.createElement('option')
+    item.text = `Valor ${num.value} adicionado.`
+    lista.appendChild(item)
  } else {
   window.alert('Valor inválido ou já encontrado na lista.')
  }
+
+  num.value = ''
+  num.focus()
 }
+
+function finalizar() {
+  if (valores.length == 0) {
+    window.alert('Adicione valores antes de finalizar!')
+  } else {
+    let tot = valores.length
+
+    res.innerHTML = ''
+    res.innerHTML += `<p>Ao todo, temos ${tot} números cadastrados.</p>`
+  }
+
+}
+
+
