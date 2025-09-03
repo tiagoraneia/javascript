cdf26cc51e88960eeac32aa0395219dedc3f086f
Tiago Raneia <tiago.ranea93@gmail.com>
2025-09-03 08:04:18 -0300

    atualização

--- CursoJs/aula16ex/ex018/script.js
+++ CursoJs/aula16ex/ex018/script.js
@@ -26,6 +26,7 @@ function adicionar () {
     let item = document.createElement('option')
     item.text = `Valor ${num.value} adicionado.`
     lista.appendChild(item)
+    res.innerHTML = ''
  } else {
   window.alert('Valor inválido ou já encontrado na lista.')
  }
@@ -39,9 +40,18 @@ function finalizar() {
     window.alert('Adicione valores antes de finalizar!')
   } else {
     let tot = valores.length
-
+    let maior = valores[0]
+    let menor = valores[0]
+    for(let pos in valores) {
+      if (valores[pos] > maior)
+        maior = valores[pos]
+      if (valores[pos] < menor)
+        menor = valores[pos]
+    }
     res.innerHTML = ''
     res.innerHTML += `<p>Ao todo, temos ${tot} números cadastrados.</p>`
+    res.innerHTML += `<p>O maior valor informado foi ${maior}<p/>`
+    res.innerHTML += `<p>O maior valor informado foi ${menor}<p/>`
   }
 
 }
