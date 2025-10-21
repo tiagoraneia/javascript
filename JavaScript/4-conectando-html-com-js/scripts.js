469db1db9b6aad36946b07e7688e1002a0f0d2db
Tiago Raneia <tiago.ranea93@gmail.com>
2025-10-21 16:05:34 -0300

    inicio das aulas

---
+++ JavaScript/4-conectando-html-com-js/scripts.js
@@ -0,0 +1,6 @@
+// Exibe uma mensagem no console do navegador.
+// O "console" é uma ferramenta usada para depuração (debug) e testes rápidos.
+console.log("Hello Word!");
+
+// Mostra outra mensagem confirmando que o arquivo JS foi corretamente vinculado ao HTML.
+console.log("Essa mensagem é do JS conectado com o HTML");
---
+++ JavaScript/classroom/scripts.js
@@ -0,0 +1,2 @@
+console.log("Hello Word!")
+console.log("Essa mensagem é do JS conectado com o HTML")
