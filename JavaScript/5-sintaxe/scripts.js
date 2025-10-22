823f7a963edff1dcc294f59dcbaa4342bf61ad65
Tiago Raneia <tiago.ranea93@gmail.com>
2025-10-22 15:24:34 -0300

    sintaxe

---
+++ JavaScript/5-sintaxe/scripts.js
@@ -0,0 +1,9 @@
+// Sintaxe correta.
+console.log("Olá, Rodrigo!")
+
+// Sintaxe incorreta:
+// con.sole.log("Olá, Rodrigo!")
+
+// No Javascript ponto e vírgula é opcional.
+console.log("Olá, João");
+console.log("Olá, João")
