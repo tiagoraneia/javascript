6a840d3e848931a709bdc0c1833fb9630c5205da
Tiago Raneia <tiago.ranea93@gmail.com>
2025-10-24 14:47:00 -0300

    const

---
+++ JavaScript/8-const/scripts.js
@@ -0,0 +1,8 @@
+
+// Uma constante cria uma variável cujo valor é fixo (não pode ser alterado)
+const number = 42
+console.log(number)
+
+// Se fizer como no exemplo abaixo vai dar erro.
+number = 55
+console.log(number)
