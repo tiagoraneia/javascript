b88f9477d9f580ebcad50fa1f504ad56beecd63f
Tiago Raneia <tiago.ranea93@gmail.com>
2025-10-24 09:53:03 -0300

    atualização let

--- JavaScript/7-let/scripts.js
+++ JavaScript/7-let/scripts.js
@@ -1,2 +1,15 @@
-console.log("Hello Word!")
-console.log("Essa mensagem é do JS conectado com o HTML")
+// Declara uma variável sem valor.
+
+let user
+console.log("Rodrigo Gonçalves")
+
+// Não permite criar com o mesmo nome usado anteriormente.
+
+// Declara uma variável com valor.
+let email = "rodrigo@email.com"
+console.log(email)
+
+// Mas é possível também substituir o conteúdo da variável:
+
+email = "joao@email.com"
+console.log(email)
