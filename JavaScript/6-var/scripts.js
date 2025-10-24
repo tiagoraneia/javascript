2f64b6bbd46b26b848f5d0059fb0b85e0b9a9179
Tiago Raneia <tiago.ranea93@gmail.com>
2025-10-24 08:57:23 -0300

    atualização

---
+++ JavaScript/6-var/scripts.js
@@ -0,0 +1,18 @@
+// Declara uma variável sem o valor.
+
+// ***  var user
+
+// Exibe o conteúdo de usar:
+
+// ***  console.log(user)
+
+// Declarei acima uma variável de nome user que não tem nenhum conteúdo.
+// Mas posso tambem declarar uma variável com valor:
+
+var email = "tiago.ranea93@gmail.com"
+console.log(email)
+
+// Podemos substituir o valor da variável:
+
+email = "joao@email.com"
+console.log(email)
