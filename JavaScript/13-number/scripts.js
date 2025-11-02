efa819fe72c6cd842520581ddd591b397ff5a058
Tiago Raneia <tiago.ranea93@gmail.com>
2025-11-02 10:27:08 -0300

    number

---
+++ JavaScript/13-number/scripts.js
@@ -0,0 +1,13 @@
+// Number
+console.log(typeof 5)
+
+// Inteiro positivo.
+console.log(5)
+
+// Inteiro negativo.
+console.log(-5)
+
+// Número reais ou float. (não usar virgula)
+console.log(125.70)
+
+
