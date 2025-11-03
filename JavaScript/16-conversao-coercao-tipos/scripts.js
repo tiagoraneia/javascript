a9f48dbdb3fa907470663073b26bd22c7ac97654
Tiago Raneia <tiago.ranea93@gmail.com>
2025-11-03 18:00:18 -0300

    atualizacao

--- JavaScript/16-conversao-coercao-tipos/scripts.js
+++ JavaScript/16-conversao-coercao-tipos/scripts.js
@@ -1,7 +1,30 @@
 /*
   - Coerção de tipos (type casting ou type conversion):
   ocorre quando você explicitamente transforma um valor de um tipo para outro. Isso é feito de forma consciente, usando funções ou métodos específicos para realizar a conversão.
+*/
 
-  - Coerção de tipos:
+// Converter para número.
+let value = "9"
+console.log(typeof value)
+console.log(typeof Number(value))
+
+
+// Converter para texto.
+let age = 18
+console.log(typeof age.toString())
+console.log(typeof String(age))
+
+
+let option = 1
+console.log(Boolean(option))
+console.log(typeof Boolean(option))
+
+
+
+/*
+- Coerção de tipos:
   aconrece de forma automática (implicitamente). O JavaScript tenta automaticamente converter um dos valores para um tipo compatível antes de realizar a operação.
 */
+
+// Exemplo de coerção.
+console.log(typeof ("10" + 5))
