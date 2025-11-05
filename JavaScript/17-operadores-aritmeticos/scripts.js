4e7b08923c4b6c1354b033c0fa3f64e97edccedb
Tiago Raneia <tiago.ranea93@gmail.com>
2025-11-05 10:07:48 -0300

    operadores-aritimeticos

---
+++ JavaScript/17-operadores-aritmeticos/scripts.js
@@ -0,0 +1,14 @@
+// Operadores aritméticos
+
+console.log("SOMA: ", 12 + 8)
+console.log("CONCATENAÇÃO: ", "12" + "8")
+
+console.log("SUBTRAÇÃO: ", 12 - 10)
+
+console.log("MULTIPLICAÇÃO: ", 3 * 5.5)
+
+console.log("DIVISÃO: ", 12 / 2)
+
+console.log("RESTO DA DIVISÃO: ", 13 % 2)
+
+console.log("EXPONENCIAL: ", 3 ** 3)
