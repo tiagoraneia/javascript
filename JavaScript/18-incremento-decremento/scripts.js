8d0a97b570e7bf1150a9fcd344c1ae15e5d59d04
Tiago Raneia <tiago.ranea93@gmail.com>
2025-11-05 12:42:37 -0300

    incremento e decremento

---
+++ JavaScript/18-incremento-decremento/scripts.js
@@ -0,0 +1,36 @@
+let number = 10
+
+// Incremento
+// number = number +1
+number++
+
+// Incrementa após (por isso não mostra no console)
+console.log(number++)
+console.log(number)
+
+// Incrementa antes
+console.log(++number)
+
+// Decremento - segue a mesma ideia.
+// console.log("Decremento após: ", number--)
+// console.log(number--)
+console.log("Decremento antes:", --number)
+
+// Incrementar mais de um (colocar).
+// Adiciona 10 (exemplo)
+number += 10
+console.log(number)
+
+// Decrementar mais de um (remover).
+// Tira 2 (exemplo)
+number -= 2
+console.log(number)
+
+// Seria a mesma coisa de fazer isso com +
+number = number + 20
+console.log(number)
+
+// E fazer isso com -
+number = number - 20
+console.log(number)
+
