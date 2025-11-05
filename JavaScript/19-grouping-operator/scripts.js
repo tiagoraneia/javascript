796b4b4245861d19b75eb2095ca75d03d53c98cf
Tiago Raneia <tiago.ranea93@gmail.com>
2025-11-05 17:15:47 -0300

    igual

---
+++ JavaScript/19-grouping-operator/scripts.js
@@ -0,0 +1,10 @@
+// Grouping Operator (ordem de prescedência)
+
+let total1 = 2 + 3 * 4
+console.log(total1)
+
+let total2 = (2 + 3) * 4
+console.log(total2)
+
+let average = (9.5 + 7 + 5) / 3
+console.log(average)
