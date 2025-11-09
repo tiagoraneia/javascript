bc00d9e01784da6983c18130b82625cb5d67508b
Tiago Raneia <tiago.ranea93@gmail.com>
2025-11-08 21:45:55 -0300

    final terceiro módulo

---
+++ JavaScript/23-operadores-atribuição/scripts.js
@@ -0,0 +1,29 @@
+let value
+
+// Operador atribuição
+value = 1
+console.log(value)
+
+// Incremento
+value += 2
+console.log(value)
+
+// Decremento
+value -= 1
+console.log(value)
+
+// Multiplicar
+value *= 3
+console.log(value)
+
+//Divisão
+value /= 2
+console.log(value)
+
+// Resto da divisão
+// value %= 2
+// console.log(value)
+
+// Potência
+value **= 2
+console.log(value)
---
+++ JavaScript/24-operadores-logicos/scripts.js
@@ -0,0 +1,12 @@
+let email = true
+let password = false
+let isAdmin = true
+
+// AND (E) && - ambos tem que ser verdadeiros
+console.log(email && password && isAdmin)
+
+// OR (OU) ||
+console.log(email || password) 
+
+// NOT (negação) !
+console.log(!password)
