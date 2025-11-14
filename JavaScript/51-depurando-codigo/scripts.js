71fd804129474dc93fef9415843bbfa20f830796
Tiago Raneia <tiago.ranea93@gmail.com>
2025-11-14 08:58:45 -0300

    depurando codigo

---
+++ JavaScript/51-depurando-codigo/scripts.js
@@ -0,0 +1,9 @@
+const input = document.querySelector("input")
+const form = document.querySelector("form")
+
+form.onsubmit = (event) => {
+  event.preventDefault()
+
+  const value = input.value
+  const regex = 
+}
