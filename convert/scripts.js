54c7c579b0b8e7c42b00c1aa4c8af1620453b673
Tiago Raneia <tiago.ranea93@gmail.com>
2025-11-15 19:36:19 -0300

    atualização

--- convert/scripts.js
+++ convert/scripts.js
@@ -7,6 +7,9 @@ const GBP = 6.08
 const form = document.querySelector("form")
 const amount = document.getElementById("amount")
 const currency = document.getElementById("currency")
+const footer = document.querySelector("main footer")
+const description = document.getElementById("description")
+const result = document.getElementById("result")
 
 // Manipulando o input amount para receber somente números.
 amount.addEventListener("input", () => {
@@ -33,5 +36,40 @@ form.onsubmit = (event) => {
 
 // Função para converter a moeda.
 function convertCurrency(amount, price, symbol) {
-  console.log(amount, price, symbol)
+  try {
+    // Exibindo a cotação da moeda selecionada.
+    description.textContent = `${symbol} 1 = ${formatCurrencyBRL(price)}`
+
+    // Calcula o total.
+    let total = amount * price 
+   
+    // Verifica se o resultado não é um número.
+    if(isNaN(total)){
+      return alert("Por favor, digite o valor corretamente para converter.")
+    }
+
+    // Formatar o valor total.
+    total = formatCurrencyBRL(total).replace("R$", "")
+
+    // Exibe o resultado total.
+    result.textContent = `${total} Reais` 
+    
+    // Aplica a classe que exibe o footer para mostrar o resultado.
+    footer.classList.add("show-result")
+  } catch (error) {
+    // Remove a classe do footer removendo ele da tela.
+    footer.classList.remove("show-result")
+
+    console.log(error)
+    alert("Não foi possível converter. Tente novamente mais tarde.")
+  }
 }
+
+// Formata a moeda em Real Brasileiro.
+function formatCurrencyBRL(value) {
+  // Converte para número para utilizar o LocaleString para formatar no padrão BRL (R$ 00,00).
+  return Number(value).toLocaleString("pt-BR", {
+    style: "currency",
+    currency:"BRL",
+  })
+} 
